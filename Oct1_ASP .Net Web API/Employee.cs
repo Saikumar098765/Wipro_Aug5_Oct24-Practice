593accbc879553a4ec0cbe2a75c6035965c32706
Saikumar098765 <saikumaruruvakili@gmail.com>
2024-10-01 15:58:45 +0530

    Add files via upload

---
+++ Oct1_ASP .Net Web API/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restful_Web_API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - DateOfBirth.Year;
+            }
+        }
+    }
+
+}
